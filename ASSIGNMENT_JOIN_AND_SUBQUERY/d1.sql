b6bec9ff266146c556d1dce4c1c7c175217a228c
Likesh Chaudhari <98093266+likesh123chaudhari@users.noreply.github.com>
2025-04-08 07:09:42 -0700

    Add files via upload

---
+++ ASSIGNMENT_JOIN_AND_SUBQUERY/d1.sql
@@ -0,0 +1,6 @@
+delimiter $
+create  procedure pro1()
+begin
+select 'hello world ' as "message box";
+end $
+delimiter ;
---
+++ ASSIGNMENT_JOIN_AND_SUBQUERY/demo.s
@@ -0,0 +1,6 @@
+delimeter$
+create procedure pro1() 
+begin 
+select * from emp; 
+end$
+delimeter;
